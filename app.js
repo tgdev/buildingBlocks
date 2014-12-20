5af386e1c3728458ac08166a33df8ea4ff95b4f0
Tom Gillard <tomgillar@gmail.com>
2014-12-20 15:37:38 +1100

    initial commit with express api

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+// app.use(express.static({}));
+
+app.get('/', function(req, res) {
+	res.send('Hello yall');
+});
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
